4337438178d95ea3ef1c00cf33d2a0341bbbab2c
RabbitRPA <59790840+RabbitRPA@users.noreply.github.com>
2020-02-10 19:39:36 +0100

    Update Zadanie1.py

--- Zadanie1.py
+++ Zadanie1.py
@@ -13,7 +13,7 @@ def cat_dog(str= ''):
     return cat == dog
             
   """
-
+"""
   def count_code(str):
     ile = 0
 
@@ -31,3 +31,36 @@ def cat_dog(str= ''):
         if str[i:i+4] == "cole":
             ile += 1
     return ile
+"""
+
+Id = {
+    0: 1,
+    1: 3,
+    2: 7,
+    3: 9,
+    4: 1,
+    5: 3,
+    6: 7,
+    7: 9,
+    8: 1,
+    9: 3,
+    10: 1
+}
+
+#last number is zero it True
+
+how_may = int(input())
+soultion = []
+
+
+while how_may > 0:
+    how_may -= 1
+    soultion.clear()
+    pesel = list(map(int, input()))
+    for i in range(0, 11):
+        soultion.append(Id[i]* pesel[i])
+    if sum(soultion) % 10 == 0 :
+        print("D")
+    else:
+        print("N") 
+
