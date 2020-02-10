0725d6c966578486c0589b5cdee0822be96cd588
RabbitRPA <59790840+RabbitRPA@users.noreply.github.com>
2020-02-10 19:36:37 +0100

    Update Zadanie1.py

--- Zadanie1.py
+++ Zadanie1.py
@@ -1,3 +1,4 @@
+"""
 def cat_dog(str= ''):
     cat = 0
     dog = 0
@@ -11,4 +12,22 @@ def cat_dog(str= ''):
         
     return cat == dog
             
-  
+  """
+
+  def count_code(str):
+    ile = 0
+
+    for i in range(len(str)):
+        if str[i:i+4] == "code":
+            ile += 1
+        if str[i:i+4] == "cope":
+            ile += 1
+        if str[i:i+4] == "coze":
+            ile += 1  
+        if str[i:i+4] == "cooe":
+            ile += 1
+        if str[i:i+4] == "core":
+            ile += 1
+        if str[i:i+4] == "cole":
+            ile += 1
+    return ile
