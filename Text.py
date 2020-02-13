853c977b560f7efccd6bf3a0ec1b2582c63cfcaf
RabbitRPA <59790840+RabbitRPA@users.noreply.github.com>
2020-02-13 17:00:16 +0100

    Create Text.py

---
+++ Text.py
@@ -0,0 +1,10 @@
+listaGosci = {
+                ('Arkadiusz', 28, 'mężczyzna'),
+                ('Wioletta', 22, 'kobieta'),
+                ('Kuba', 32, 'mężczyzna')  
+             }
+listaGosci2 = {
+                ('Arkadiusz', 28, 'mężczyzna'),
+                ('W', 22, 'kobieta'),
+                ('K', 32, 'mężczyzna')  
+             }
