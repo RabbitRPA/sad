6bfc86164f564066ab97fa2633e5fca753be85f6
RabbitRPA <59790840+RabbitRPA@users.noreply.github.com>
2020-02-13 17:30:14 +0100

    Update Text.py

--- Text.py
+++ Text.py
@@ -1,10 +1,12 @@
-listaGosci = {
-                ('Arkadiusz', 28, 'mężczyzna'),
-                ('Wioletta', 22, 'kobieta'),
-                ('Kuba', 32, 'mężczyzna')  
-             }
-listaGosci2 = {
-                ('Arkadiusz', 28, 'mężczyzna'),
-                ('W', 22, 'kobieta'),
-                ('K', 32, 'mężczyzna')  
-             }
+people = {
+        "IcFDG2bO9AYDF651DoyH": {'name': 'John', 'age': 27, 'sex': 'Male'},
+        "KcD9ntE6IRM59vkVta1k": {'name': 'Marie', 'age': 22, 'sex': 'Female'},
+        "yDlgcn99xPc19mYXcRmy": {'name': 'Agness', 'age': 25, 'sex': 'Female'},
+        "cpQh6GiAbBdGv35NDoTk": {'name': 'Nabeel', 'age': 17, 'sex': 'Male'},
+        "12BifzWxCQySKgLhgahC": {'name': 'Jasmin ', 'age': 42, 'sex': 'Female'},
+        "QLnmg0pzlLj9x7c7DlLv": {'name': 'Ruby', 'age': 55, 'sex': 'Female'},
+        "To47urX0DUznWmOxGZ6H": {'name': 'Lori', 'age': 27, 'sex': 'Male'},
+        "KQ4bir3y4tlkbG69I7zq": {'name': 'Marie', 'age': 42, 'sex': 'Female'},
+        "94cp4hsyZP2BnCh4D34z": {'name': 'Agness', 'age': 25, 'sex': 'Female'},
+        "Vr4wRdkljeEs46Czxo54": {'name': 'Chiara', 'age': 17, 'sex': 'Male'},          
+         }
